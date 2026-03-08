40833aeb15c32d842aacf6b4840331adaf15eed0
eroscedere <rafael1501.pack@gmail.com>
2026-03-08 16:39:39 -0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -7,14 +7,20 @@ if(!url) return
 const res = await fetch(`/info?url=${encodeURIComponent(url)}`)
 const data = await res.json()
 
+const id = Math.random().toString(36).substring(7)
+
 const container = document.createElement("div")
 container.className = "video-item"
+container.id = id
 
 container.innerHTML = `
 <img src="${data.thumbnail}">
 <div>
 <p>${data.title}</p>
-<button onclick="downloadVideo('${data.url}')">Download</button>
+<button onclick="downloadVideo('${data.url}','${id}')">Download</button>
+<div class="progress-bar">
+<div class="progress" id="progress-${id}"></div>
+</div>
 </div>
 `
 
@@ -24,10 +30,40 @@ document.getElementById("videoUrl").value = ""
 
 }
 
-function downloadVideo(url){
+
+async function downloadVideo(url,id){
+
+const res = await fetch(`/download?url=${encodeURIComponent(url)}`)
+const data = await res.json()
+
+const downloadId = data.download_id
+
+checkProgress(downloadId,id,data.file)
+
+}
+
+
+async function checkProgress(downloadId,id,fileUrl){
+
+const progressBar = document.getElementById(`progress-${id}`)
+
+const interval = setInterval(async ()=>{
+
+const res = await fetch(`/progress?download_id=${downloadId}`)
+const data = await res.json()
+
+progressBar.style.width = data.progress + "%"
+
+if(data.progress >= 100){
+
+clearInterval(interval)
 
 const link = document.createElement("a")
-link.href = `/download?url=${encodeURIComponent(url)}`
+link.href = fileUrl
 link.click()
+
+}
+
+},1000)
 
 }
