1ae4e8a829ff51f67cf58f1f2719ff19b5b6cb88
eroscedere <rafael1501.pack@gmail.com>
2026-03-08 16:11:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import yt_dlp
 import os
+import uuid
 
 app = FastAPI()
 
@@ -11,9 +12,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 DOWNLOAD_FOLDER = "downloads"
-
-if not os.path.exists(DOWNLOAD_FOLDER):
-    os.makedirs(DOWNLOAD_FOLDER)
+os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -41,13 +40,20 @@ def get_video_info(url: str):
 @app.get("/download")
 def download_video(url: str):
 
+    file_id = str(uuid.uuid4())
+    filepath = f"{DOWNLOAD_FOLDER}/{file_id}.mp4"
+
     ydl_opts = {
         "format": "bestvideo+bestaudio/best",
         "merge_output_format": "mp4",
-        "outtmpl": f"{DOWNLOAD_FOLDER}/%(title)s.%(ext)s"
+        "outtmpl": filepath
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
-    return {"status": "download started"}
+    return FileResponse(
+        path=filepath,
+        filename="video.mp4",
+        media_type="application/octet-stream"
+    )
